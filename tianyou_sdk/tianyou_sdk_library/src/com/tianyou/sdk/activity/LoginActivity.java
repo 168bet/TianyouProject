427a466cb58af517392473f1e28725cb1d5a063c
itstrongs <itstrongs@163.com>
2017-03-13 15:04:40 +0800

    liu:

--- tianyou_sdk/tianyou_sdk_library/src/com/tianyou/sdk/activity/LoginActivity.java
+++ tianyou_sdk/tianyou_sdk_library/src/com/tianyou/sdk/activity/LoginActivity.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
@@ -111,9 +112,9 @@ public class LoginActivity extends BaseActivity implements ConnectionCallbacks, 
 		.addScope(Plus.SCOPE_PLUS_LOGIN).addConnectionCallbacks(this).addOnConnectionFailedListener(this).build();
 	}
 	
-	public void logoutFacebook() {
-		btnLogin.performClick();
-	}
+//	public void logoutFacebook() {
+//		btnLogin.performClick();
+//	}
 	
 	private LoginButton btnLogin;
 	
@@ -187,7 +188,13 @@ public class LoginActivity extends BaseActivity implements ConnectionCallbacks, 
 				switchFragment(AccountFragment.getInstance(isSwitchAccount), "AccountFragment");
 			}
 		}
-		if (isSwitchAccount && ConfigHolder.isOverseas) {
+		if (ConfigHolder.isOverseas) {
+			AccessToken token = AccessToken.getCurrentAccessToken();
+			if (token != null) {
+				LogUtils.d("token:" + token.getToken());
+			}
+		}
+		if (isSwitchAccount && ConfigHolder.isOverseas && AccessToken.getCurrentAccessToken() != null) {
 			clickFacebook();
 		}
 	}
