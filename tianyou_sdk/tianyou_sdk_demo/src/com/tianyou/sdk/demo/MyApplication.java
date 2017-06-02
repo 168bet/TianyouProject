3610ecf42f441ac8fcb8f2a1b3fcc0c5ac2403b5
itstrongs <itstrongs@163.com>
2017-06-02 20:02:59 +0800

    liu

--- tianyou_sdk/tianyou_sdk_demo/src/com/tianyou/sdk/demo/MyApplication.java
+++ tianyou_sdk/tianyou_sdk_demo/src/com/tianyou/sdk/demo/MyApplication.java
@@ -9,8 +9,8 @@ public class MyApplication extends Application {
 	@Override
 	public void onCreate() {
 		super.onCreate();
-		String gameId = "11005";
-		String gameToken = "b0a4fed42fc9723fc5ef166da6e25614";
+		String gameId = "131";
+		String gameToken = "1afa34a7f984eeabdbb0a7d494132ee5";
 		String gameName = "测试";
 		/**
 		 * gameId：app唯一标识，非常重要，请认真填写，确保正确
