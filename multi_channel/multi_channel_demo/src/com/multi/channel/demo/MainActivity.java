16711980b81bbfe70e987941ccc103147c3f8f2f
itstrongs <itstrongs@163.com>
2017-06-20 15:21:06 +0800

    kakao接入

--- multi_channel/multi_channel_demo/src/com/multi/channel/demo/MainActivity.java
+++ multi_channel/multi_channel_demo/src/com/multi/channel/demo/MainActivity.java
@@ -1,6 +1,5 @@
 package com.multi.channel.demo;
 
-import com.gamebegin.smcsj.R;
 import com.tianyou.channel.bean.PayParam;
 import com.tianyou.channel.bean.RoleInfo;
 import com.tianyou.channel.interfaces.BaseSdkService;
@@ -8,16 +7,13 @@ import com.tianyou.channel.interfaces.TianyouCallback;
 import com.tianyou.channel.interfaces.TianyouSdk;
 import com.tianyou.channel.utils.LogUtils;
 import com.tianyou.channel.utils.ToastUtils;
-<<<<<<< HEAD
 import com.tianyouxi.lszg.leshi.R;
-=======
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
->>>>>>> 9e502e78ae7e1731a84031db0dd8e05524d67bc1
 
 public class MainActivity extends Activity implements OnClickListener {
 
