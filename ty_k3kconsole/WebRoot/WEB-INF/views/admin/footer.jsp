20c556fd230271d76a1f0b70a65a1cdee3376957
itstrongs <itstrongs@163.com>
2017-07-03 18:04:16 +0800

    slg

--- ty_k3kconsole/WebRoot/WEB-INF/views/admin/footer.jsp
+++
@@ -1,3 +0,0 @@
-<%@ page language="java" import="java.util.*" pageEncoding="UTF-8"%>
-
-<div class="footer text-center">K3KConsole ©苏ICP备05019125号-10</div>
--- ty_k3kconsole/src/com/k3k/unplat/dao/LoginUserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.k3k.unplat.dao;
-
-
-import com.k3k.unplat.entity.LoginUserEntity;
-
-public interface LoginUserMapper {
-
-	LoginUserEntity selectByUserName(String userName);
-}
